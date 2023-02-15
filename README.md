1a3ddcfa025f7a74388bd85329e3502cc336f3fd
glaubermatos <glaub.oliveira@hotmail.com>
2023-02-15 10:12:23 -0300

    Estrutura visual - Configurando Stitches

---
+++ README.md
@@ -0,0 +1,3 @@
+Capítulo 2: Estrutura visual - Configurando Stitches
+
+Nessa aula conheceremos o Stitches, uma ferramenta CSS-in-JS com foco em performance e experiencia de desenvolvimento que utilizaremos para estilizar a nossa aplicação.
---
+++ src/pages/index.tsx
@@ -0,0 +1,26 @@
+import { styled } from "../styles"
+
+const Button = styled('button', {
+  backgroundColor: '$rocketseat',
+  borderRadius: 4,
+  padding: '4px 8px',
+  border: 0,
+
+  span: {
+    fontWeight: 'bold',
+    marginRight: 8
+  },
+
+  '&:hover': {
+    filter: 'brightness(0.8)'
+  }
+})
+
+export default function Home() {
+  return (
+    <Button>
+      <span>Teste</span>
+      Enviar
+    </Button>
+  )
+}
